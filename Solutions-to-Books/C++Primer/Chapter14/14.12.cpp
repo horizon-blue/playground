cc919d70242d40657691365950efbab490b0ae15
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2017-01-30 01:17:10 -0600

    Finish 14.2

--- Solutions-to-Books/C++Primer/Chapter14/14.12.cpp
+++ Solutions-to-Books/C++Primer/Chapter14/14.12.cpp
@@ -1,11 +1,12 @@
-// Exercise 14.08
-// Define an outout operator for the class you chose in exercise 7.40
-// Xiaoyan Wang 01/28/2017
+// Exercise 14.12
+// Define an input operator for the class you used in exercise 7.40 from Section
+// 7.5.1. Be sure the operator handles input errors.
+// Xiaoyan Wang 01/30/2017
 
-// Written based on 7.40.cpp
+// Written based on 14.08.cpp
 
-#ifndef __14_08_CPP__
-#define __14_08_CPP__
+#ifndef __14_12_CPP__
+#define __14_12_CPP__
 
 #include <iostream>
 #include <string>
@@ -26,6 +27,7 @@ public:
     Book(const Book& other) = default;
 
     friend std::ostream& operator<<(std::ostream&, const Book&);
+    friend std::istream& operator>>(std::istream&, Book&);
 
 private:
     string title;
@@ -38,6 +40,25 @@ private:
 std::ostream& operator<<(std::ostream& os, const Book& b) {
     os << b.title << ", " << b.author << ", " << b.price;
     return os;
+}
+
+std::istream& operator>>(std::istream& is, Book& b) {
+    if(!(is >> b.title >> b.price >> b.isbn >> b.author)) {
+        b = Book();
+    } else {
+        if(b.price < 0) {
+            is.setstate(std::istream::failbit);
+            b = Book();
+        }
+    }
+    return is;
+}
+
+int main() {
+    Book b;
+    std::cin >> b;
+    std::cout << b << std::endl;
+    return 0;
 }
 
 #endif
